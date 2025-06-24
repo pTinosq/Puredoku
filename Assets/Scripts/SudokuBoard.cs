ca0a58f4a2145b6e87c3753479392dae9f535cb2
pTinosq <tinosps@gmail.com>
2025-06-24 22:00:01 +0100

    Reset cache

---
+++ Assets/Scripts/SudokuBoard.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class SudokuBoard : MonoBehaviour
+{
+    public int[] board = new int[9 * 9];
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }   
+    void Update() {
+
+    }
+}
